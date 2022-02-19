b4a2e2c210505ceb14746696efac041301da56c6
Magda <meg.nowak27@gmail.com>
2022-02-19 20:32:57 +0100

    created one function to all animations

--- js/app.js
+++ js/app.js
@@ -3,50 +3,26 @@ gsap.registerPlugin(ScrollTrigger);
 const servicesTitles = document.querySelectorAll('.services__text-title');
 const servicesIcons = document.querySelectorAll('.services__icon-wrapper');
 const servicesLists = document.querySelectorAll('.services__text-list');
+const parentClass = ".services__text-wrapper";
 
-servicesTitles.forEach(title => {
-    let parentElement = title.closest(".services__text-wrapper");
-    gsap.to(title, {
-        scrollTrigger: {
-            trigger: parentElement,
-            start: "50vh center",
-            markers: true,
-        },
-        x: "0%",
-        opacity: 1,
-        duration: 0.8,
-        ease: 'power3.inOut'
+function gsapAnimation(elements, duration ) {
+    elements.forEach(element => {
+        let parentElement = element.closest(parentClass);
+        gsap.to(element, {
+            scrollTrigger: {
+                trigger: parentElement,
+                start: "50vh center",
+                markers: true,
+            },
+            x: "0%",
+            y: "0%",
+            opacity: 1,
+            duration: duration,
+            ease: 'power3.inOut'
+        })
     })
-})
+}
 
-servicesIcons.forEach(icon => {
-    let parentElement = icon.closest(".services__text-wrapper");
-    gsap.to(icon, {
-        scrollTrigger: {
-            trigger: parentElement,
-            start: "50vh center",
-            markers: true,
-        },
-        x: "0%",
-        opacity: 1,
-        duration: 0.2,
-        ease: 'power3.inOut'
-    })
-})
-
-servicesLists.forEach(list => {
-    let parentElement = list.closest(".services__text-wrapper");
-    gsap.to(list, {
-        scrollTrigger: {
-            trigger: parentElement,
-            start: "50vh center",
-            markers: true,
-        },
-        y: "0%",
-        opacity: 1,
-        duration: 1.5,
-
-        ease: 'power3.inOut'
-
-    })
-})
+gsapAnimation(servicesTitles,  0.8);
+gsapAnimation(servicesIcons,  0.4);
+gsapAnimation(servicesLists,  0.8);
